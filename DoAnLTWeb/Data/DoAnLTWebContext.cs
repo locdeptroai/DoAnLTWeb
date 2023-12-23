0ab91bafe39737910026a9e590089cb435464b4f
Nhom 5 <86753838+locdeptroai@users.noreply.github.com>
2023-12-24 00:06:13 +0700

    Add project files.

---
+++ DoAnLTWeb/Data/DoAnLTWebContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DoAnLTWeb.Models;
+
+namespace DoAnLTWeb.Data
+{
+    public class DoAnLTWebContext : DbContext
+    {
+        public DoAnLTWebContext (DbContextOptions<DoAnLTWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DoAnLTWeb.Models.Category> Category { get; set; } = default!;
+
+        public DbSet<DoAnLTWeb.Models.Product>? Product { get; set; }
+
+        public DbSet<DoAnLTWeb.Models.User>? User { get; set; }
+    }
+}
---
+++ DoAnLTWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoAnLTWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
